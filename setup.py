be48016e3720593762b022d883ce27d74576a23f
Kalpesh-Rajput <kr342803@gmail.com>
2024-09-22 19:41:06 +0530

    Logging and exception file

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
-from setuptools import find_packages, setup
+from setuptools import find_packages,setup
+from typing import List
 
-def get_requirements(file_path):
-    """
-    Reads the requirements from a given file and returns them as a list.
-    """
-    with open(file_path, 'r') as file:
-        requirements = file.read().splitlines()
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
     return requirements
 
 setup(
-    name='mlproject',
-    version='0.0.1',
-    author='Kalpesh-Rajput',
-    author_email='kr342803@gmail.com',
-    packages=find_packages(),  # Automatically find packages
-    install_requires=get_requirements('requirements.txt')
+name='mlproject',
+version='0.0.1',
+author='Kalpesh-Rajput',
+author_email='kr342803@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
 )
